bfa7954804eea2f2a237b9b7c18ca62fb4a62ac2
GloryOfNight <siarhei.dziki@gmail.com>
2019-09-18 13:03:45 +0300

    added game object creation for main loop

--- example-game/src/game_main.cxx
+++ example-game/src/game_main.cxx
@@ -1,20 +1,27 @@
 #include "../../dreco-engine/src/core/engine.hxx"
+#include "../../dreco-engine/src/game_objects/game_base.hxx"
+#include <iostream>
 
-int main() 
-{       
-    dreco::engine* engine = new dreco::engine();
+int main()
+{
+	dreco::engine* engine = new dreco::engine();
 
-    auto engine_properties = dreco::engine_properties();
-    engine_properties.window_title = "Example Game";
+	auto engine_properties = dreco::engine_properties();
+	engine_properties.window_title = "Example Game";
 
-    const auto init_res = engine->Init(engine_properties);
-    
-    if (init_res == dreco::INIT_SUCCESS) 
-    {
-        engine->StartMainLoop();
-    }
+	const auto init_res = engine->Init(engine_properties);
 
-    delete engine;
-    
-    return 0; 
+	if (init_res == dreco::INIT_SUCCESS)
+	{
+		dreco::game_base* game = new dreco::game_base(*engine);
+		game->Init();
+		engine->RegisterOwnedGame(game);
+		engine->StartMainLoop();
+		// on main loop finished
+		delete game;
+	}
+
+	delete engine;
+
+	return 0;
 }
