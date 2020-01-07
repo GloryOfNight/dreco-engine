6a8bd2ffc8f2b6f9fe0d4bc1890cc1fed60dde9b
GloryOfNight <siarhei.dziki@gmail.com>
2020-01-08 02:17:28 +0300

    - fix for gitignore and android improvements

--- android/app/src/main/java/com/gloryofnight/examplegame/MainActivity.java
+++ android/app/src/main/java/com/gloryofnight/examplegame/MainActivity.java
@@ -9,5 +9,19 @@ import org.libsdl.app.SDLActivity;
 
 public class MainActivity extends SDLActivity
 {
+    @Override
+    protected String getMainFunction() {
+        return "android_main";
+    }
 
+    @Override
+    protected String[] getLibraries() {
+        return new String[]{
+                "hidapi",
+                "SDL2",
+                "SDL2_image",
+                "dreco-engine",
+                "example-game"
+        };
+    }
 }
